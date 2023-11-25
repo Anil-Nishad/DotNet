9ec479b0f8c44a8d33a8ea459c764c79987b0d95
AnilK <delhi2013guddu@gmail.com>
2023-11-25 12:57:22 +0530

    Add project files.

---
+++ DotNet.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DotNet.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        // To create Constructor
+        //ctor + double Tab
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
